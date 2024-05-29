f1d4f94ce75f8f9db701892348a10048ba819be4
qwq140 <qwq140@gmail.com>
2024-05-29 15:14:52 +0900

    [react-todo] 날짜 적용 및 카운트 수정

--- react-todo/src/components/TodoHeader.js
+++ react-todo/src/components/TodoHeader.js
@@ -1,10 +1,13 @@
 export function TodoHeader({todos}) {
 
-    const count = todos.filter((e) => e.isDone).length;
+    const count = todos.filter((e) => !e.isDone).length;
+
+    const today = new Date();
+
 
     return (
         <div className={"header"}>
-            <h1>2024년 05월 29일</h1>
+            <h1>{today.getFullYear()}년 {today.getMonth()+1}월 {today.getDate()}일</h1>
             <div>할 일 : {count}개</div>
         </div>
     );
