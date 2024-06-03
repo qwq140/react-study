cc05b90e631328e7621386793e0fe5b57c17b4e5
qwq140 <qwq140@gmail.com>
2024-06-03 10:30:17 +0900

    [react-learn] 4.javascript in jsx with curly braces

---
+++ raact-learn/src/4_javascript_in_jsx_with_curly_braces/TodoList.js
@@ -0,0 +1,34 @@
+const today = new Date();
+function formatDate(date) {
+    return new Intl.DateTimeFormat(
+        'en-US',
+        { weekday: 'long' }
+    ).format(date);
+}
+
+const person = {
+    name: 'Gregorio Y. Zara',
+    theme: {
+        backgroundColor: 'black',
+        color: 'pink'
+    }
+};
+
+export default function TodoList() {
+    const name = 'Gregorio Y. Zara';
+    return (
+        <div style={person.theme}>
+            <h1>{person.name}'s To Do List for {formatDate(today)}</h1>
+            <img
+                className="avatar"
+                src="https://i.imgur.com/7vQD0fPs.jpg"
+                alt="Gregorio Y. Zara"
+            />
+            <ul>
+                <li>Improve the videophone</li>
+                <li>Prepare aeronautics lectures</li>
+                <li>Work on the alcohol-fuelled engine</li>
+            </ul>
+        </div>
+    );
+}
--- raact-learn/src/App.js
+++ raact-learn/src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import Gallery from "./1_first_component/Gallery";
-import TodoList from "./3_writing_markup_with_jsx/TodoList";
+import Avatar from "./4_javascript_in_jsx_with_curly_braces/Avartar";
+import TodoList from "./4_javascript_in_jsx_with_curly_braces/TodoList";
 
 function App() {
   return (
